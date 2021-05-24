be637b98d8a67261eb13795820912263bb4c9d58
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-05-24 17:39:54 -0300

    parte webhooks finalizado

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -6,6 +6,7 @@ import { stripe } from '../../../services/stripe';
 export async function saveSubscription(
     subscriptionId: string,
     customerId: string,
+    createAction = false
 ) {
     // salvar as info no banco de dados
 
@@ -30,11 +31,28 @@ export async function saveSubscription(
         price_id: subscription.items.data[0].price.id
     }
 
-    // salvar os dados da subscription no Fauna
-    await fauna.query(
-        q.Create(
-            q.Collection('subscriptions'),
-            { data: subscriptionData }
+    if (createAction) {
+        // salvar os dados da subscription no Fauna
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                { data: subscriptionData }
+            )
         )
-    )
+    } else {
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscriptionId
+                        )
+                    )
+                ),
+                { data: subscriptionData }
+            )
+        )
+    }
 }
