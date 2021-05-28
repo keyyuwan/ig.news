679ad571a355897b5a76efd82e312659b200422b
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-05-27 21:31:09 -0300

    slug + preview posts

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,44 +1,50 @@
-import { signIn, useSession } from 'next-auth/client'
-import { api } from '../../services/api'
-import { getStripeJs } from '../../services/stripe-js'
+import { signIn, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string;
+  priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+  const router = useRouter();
 
-    const [session] = useSession()
-
-    async function handleSubscribe() {
-        if (!session) {
-            signIn('github')
-            return
-        }
-
-       try {
-            // criação da checkout session
-            const response = await api.post('/subscribe')
-
-            const { sessionId } = response.data
-
-            const stripe = await getStripeJs()
-
-            await stripe.redirectToCheckout({ sessionId })
-       } catch(err) {
-           alert(err.message)
-       }
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
     }
 
-    return (
-        <button
-            type="button"
-            className={styles.subscribeButton}
-            onClick={handleSubscribe}
-        >
-            Subscribe now
-        </button>
-    )
+    if (session.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
+
+    try {
+      // criação da checkout session
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
 }
